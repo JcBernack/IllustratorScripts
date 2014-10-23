ad3a6add1e49516037d359c3a80f43084c2ba34e
JcBernack <jc.bernack@gmail.com>
2014-10-24 00:07:27 +0200

    Add export script

---
+++ SaveAllDocumentsAsSVG.jsx
@@ -0,0 +1,19 @@
+﻿try {
+    // check if any documents are open
+    if (app.documents.length == 0) throw new Error("No open documents.");
+    // set up save options
+    var options = new ExportOptionsSVG();
+    options.fontSubsetting = SVGFontSubsetting.None;
+    options.fontType = SVGFontType.SVGFONT;
+    // what does this do? =)
+    //options.sVGAutoKerning = true;
+    // save all documents as svg
+    for (var i = 0; i < app.documents.length; i++) {
+        var doc = app.documents[i];
+        var exportFile = new File(doc.name.substr(0, doc.name.lastIndexOf('.')));
+        doc.exportFile(exportFile, ExportType.SVG, options);
+    }
+    alert("Saved " + app.documents.length + " files as SVG.", "Success!");    
+} catch (e) {
+    alert("Error: " + e.message);
+}
