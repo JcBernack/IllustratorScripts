7dab684981efe6cf1bfaffd01d1cf7146e7c197c
JcBernack <jc.bernack@gmail.com>
2014-10-24 00:07:48 +0200

    Add consecutive IDs script

---
+++ ApplyConsecutiveIDs.jsx
@@ -0,0 +1,15 @@
+﻿try {
+    // check if something is selected
+    var selection = app.activeDocument.selection;
+    if (selection.length == 0) throw new Error("Nothing selected");
+    // prompt for parameters
+    var baseName = prompt("Enter base id", "id", "Apply consecutive IDs");
+    var index = parseInt(prompt("Enter start index", 0, "Apply consecutive IDs"));
+    // rename elements
+    for (var i = selection.length-1; i >= 0; i--) {
+        selection[i].name = baseName + index++;
+    }
+    alert("Renamed " + index + " elements.", "Success");
+} catch(e) {
+    alert("Error: " + e.message);
+}
