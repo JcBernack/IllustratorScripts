2031c6f8831e43e64f6f2e9188d329cc3404e705
JcBernack <jc.bernack@gmail.com>
2014-10-30 02:58:39 +0100

    Add regex replace to fix exported SVG files

---
+++ Rxrepl/README.md
@@ -0,0 +1,5 @@
+rxrepl is a Microsoft Windows command line tool to search and replace text in text files using Perl compatible regular expressions (PCRE).
+Website: https://sites.google.com/site/regexreplace/
+Author: Ross MacGregor
+
+These files are used SaveAllDocumentsAsSVG.jsx. Place them in the parent folder of the files which are saved as SVGs.
--- SaveAllDocumentsAsSVG.jsx
+++ SaveAllDocumentsAsSVG.jsx
@@ -1,19 +1,39 @@
 ﻿try {
     // check if any documents are open
     if (app.documents.length == 0) throw new Error("No open documents.");
+    // check if rxrepl.exe is found
+    var rxrepl = new File("../rxrepl.exe");
+    if (!rxrepl.exists) throw new Error("rxrepl.exe was not found at: " + rxrepl.fullName);
     // set up save options
     var options = new ExportOptionsSVG();
     options.fontSubsetting = SVGFontSubsetting.None;
     options.fontType = SVGFontType.SVGFONT;
-    // what does this do? =)
-    //options.sVGAutoKerning = true;
+    // get handle to batch file
+    var batch = new File("run_rxrepl.bat");
     // save all documents as svg
-    for (var i = 0; i < app.documents.length; i++) {
+    var n = app.documents.length;
+    for (var i = n-1; i >= 0; i--) {
         var doc = app.documents[i];
+        // save current document
+        doc.save();
+        // export document to svg
         var exportFile = new File(doc.name.substr(0, doc.name.lastIndexOf('.')));
         doc.exportFile(exportFile, ExportType.SVG, options);
+        var svgName = doc.name;
+        // close the document to prevent accidentally editing the svg instead of the ai file
+        doc.close(SaveOptions.DONOTSAVECHANGES);
+        // fix svg fonts
+        // alter: modify file in-place, i.e. input file is output file
+        // no-backup don't create backup file
+        // options: read the replace options from file ..\\_rxreplOptions.txt
+        batch.open("w");
+        batch.writeln("..\\rxrepl.exe --alter --no-backup --options ..\\_rxreplOptions.txt --file " + svgName);
+        batch.close();
+        batch.execute();
     }
-    alert("Saved " + app.documents.length + " files as SVG.", "Success!");    
+    alert("Saved " + n + " files as SVG.", "Success!");    
+    // delete the batch file
+    batch.remove();
 } catch (e) {
     alert("Error: " + e.message);
 }
