50c46864086e80774818205eb02a9ee0992d3816
JcBernack <jc.bernack@gmail.com>
2014-11-04 19:32:49 +0100

    Add fix for ID clash while renaming

--- ApplyConsecutiveIDs.jsx
+++ ApplyConsecutiveIDs.jsx
@@ -5,11 +5,18 @@
     // prompt for parameters
     var baseName = prompt("Enter base id", "id", "Apply consecutive IDs");
     var index = parseInt(prompt("Enter start index", 0, "Apply consecutive IDs"));
-    // rename elements
-    for (var i = selection.length-1; i >= 0; i--) {
-        selection[i].name = baseName + index++;
-    }
+    // temporary rename elements totally different to prevent internal ID clashes
+	// which cause the SVG export to contain cluttered IDs like "id0_1"
+	RenameAll(selection, "_SCRIPT_TEMP_ID_", 0);
+	// rename elements to the correct IDs
+	RenameAll(selection, baseName, index);
     alert("Renamed " + selection.length + " elements.", "Success");
 } catch(e) {
     alert("Error: " + e.message);
+}
+
+function RenameAll(items, baseName, index) {
+	for (var i = items.length-1; i >= 0; i--) {
+        items[i].name = baseName + index++;
+    }
 }
